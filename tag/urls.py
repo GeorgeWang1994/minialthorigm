4c9e02acd94f6e42dbf2b18cfde0717a6b6b7e3a
george <georgewang1994@163.com>
2017-10-30 21:40:07 +0800

    add like

--- tag/urls.py
+++ tag/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import url
 
-from chat import views as chat_view
+from tag import views as tag_view
 
 urlpatterns = [
-
+    url(r'^post/add/$', tag_view.create_post_tag_view),
+    url(r'^post/delete/$', tag_view.del_post_tag_view),
 ]
